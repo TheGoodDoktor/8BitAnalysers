ddab5c7aefb20ea3c7be0feaca5cd5d1a73b3183
TheGoodDoktor <mrmarkcraig@googlemail.com>
2022-12-20 17:09:47 +0000

    Added first graft of instructions and script to package them

--- Scripts/MakeDistPackage.bat
+++ Scripts/MakeDistPackage.bat
@@ -4,7 +4,7 @@ mkdir ..\Package\Loose\Configs
 mkdir ..\Package\Loose\GameData
 mkdir ..\Package\Loose\Games
 mkdir ..\Package\Loose\Pokes
-
+mkdir ..\Package\Loose\Docs
 
 copy ..\VSProject\x64\Release\SpectrumAnalyser.exe ..\Package\Loose\
 copy ..\Data\SpectrumAnalyser\imgui.ini ..\Package\Loose\
@@ -14,5 +14,7 @@ copy ..\Data\SpectrumAnalyser\GameData\RomInfo.bin ..\Package\Loose\GameData\
 copy ..\Data\SpectrumAnalyser\GameData\Starquake.bin ..\Package\Loose\GameData\
 copy ..\Data\SpectrumAnalyser\Pokes\Starquake.pok	..\Package\Loose\Pokes\
 copy ..\Data\SpectrumAnalyser\Games\Starquake.z80	..\Package\Loose\Games\
+copy ..\Docs\*.* ..\Package\Loose\Docs\
 
 7z a ..\Package\SpectrumAnalyser.zip ..\Package\Loose\.
+pause
