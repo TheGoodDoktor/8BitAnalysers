ffb89d55d55a83c23b4e8741b1403c8ad4c25d01
TheGoodDoktor <mrmarkcraig@googlemail.com>
2022-12-20 16:17:14 +0000

    Packaging script file now zips

--- Scripts/MakeDistPackage.bat
+++ Scripts/MakeDistPackage.bat
@@ -10,6 +10,9 @@ copy ..\VSProject\x64\Release\SpectrumAnalyser.exe ..\Package\Loose\
 copy ..\Data\SpectrumAnalyser\imgui.ini ..\Package\Loose\
 
 copy ..\Data\SpectrumAnalyser\Configs\Starquake.json ..\Package\Loose\Configs\
+copy ..\Data\SpectrumAnalyser\GameData\RomInfo.bin ..\Package\Loose\GameData\
 copy ..\Data\SpectrumAnalyser\GameData\Starquake.bin ..\Package\Loose\GameData\
 copy ..\Data\SpectrumAnalyser\Pokes\Starquake.pok	..\Package\Loose\Pokes\
 copy ..\Data\SpectrumAnalyser\Games\Starquake.z80	..\Package\Loose\Games\
+
+7z a ..\Package\SpectrumAnalyser.zip ..\Package\Loose\.
