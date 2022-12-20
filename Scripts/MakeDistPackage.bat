3e8fc6c03d4c7694698124d1cad6b4dafd636d5d
TheGoodDoktor <mrmarkcraig@googlemail.com>
2022-12-20 15:51:23 +0000

    Package build script

---
+++ Scripts/MakeDistPackage.bat
@@ -0,0 +1,15 @@
+
+
+mkdir ..\Package\Loose\Configs
+mkdir ..\Package\Loose\GameData
+mkdir ..\Package\Loose\Games
+mkdir ..\Package\Loose\Pokes
+
+
+copy ..\VSProject\x64\Release\SpectrumAnalyser.exe ..\Package\Loose\
+copy ..\Data\SpectrumAnalyser\imgui.ini ..\Package\Loose\
+
+copy ..\Data\SpectrumAnalyser\Configs\Starquake.json ..\Package\Loose\Configs\
+copy ..\Data\SpectrumAnalyser\GameData\Starquake.bin ..\Package\Loose\GameData\
+copy ..\Data\SpectrumAnalyser\Pokes\Starquake.pok	..\Package\Loose\Pokes\
+copy ..\Data\SpectrumAnalyser\Games\Starquake.z80	..\Package\Loose\Games\
