d8078a3ff18f079c36b5bf975c0c92b4c165c069
TheGoodDoktor <mrmarkcraig@googlemail.com>
2023-01-28 11:20:45 +0000

    CMake & GLFW working for C64. CI for C64

--- Source/Shared/ImGuiSupport/GLFW/ImGuiTexture_GLFW.cpp
+++ Source/Shared/ImGuiSupport/GLFW/ImGuiTexture_GLFW.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
+#include <vector>
 
 
 // assume it's 8bpp
@@ -47,4 +48,40 @@ void ImGui_UpdateTextureRGBA(ImTextureID texture,unsigned char* pixels)
 
 	// Restore state
 	glBindTexture(GL_TEXTURE_2D, lastTexture);
+}
+
+static std::vector<uint32_t>	g_UploadBuffer;
+
+void ImGui_UpdateTextureRGBA(ImTextureID texture, unsigned char* pixels, int srcWidth, int srcHeight)
+{
+	GLint lastTexture;
+	glGetIntegerv(GL_TEXTURE_BINDING_2D, &lastTexture);
+
+	glBindTexture(GL_TEXTURE_2D, (GLuint)(intptr_t)texture);
+	int width, height;
+	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &width);
+	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &height);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, srcWidth, srcHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+	return;
+	g_UploadBuffer.resize(width * height);
+
+	//D3D11_MAPPED_SUBRESOURCE mappedResource;
+	//HRESULT hr = pDeviceCtx->Map(pTexture, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+	//if (hr == S_OK)
+	{
+		const uint8_t* srcPtr = pixels;
+		uint8_t* destPtr = (uint8_t*)g_UploadBuffer.data();
+		for (int line = 0; line < srcHeight; line++)
+		{
+			const size_t srcStride = srcWidth * 4;// assume 32bpp
+			memcpy(destPtr, srcPtr, srcStride);	// copy one line
+
+			srcPtr += srcStride;
+			destPtr += width * 4;	// 4 bytes per
+		}
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+		
+		//pDeviceCtx->Unmap(pTexture, 0);
+	}
 }
