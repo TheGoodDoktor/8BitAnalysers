97beeed0a8e83ced6f046228c5078f0166a2a591
Colourclash <sam@colourclash.co.uk>
2023-02-12 19:58:42 +0000

    Can specify address range when exporting an ASM file

--- Source/ZXSpectrum/Exporters/AssemblerExport.h
+++ Source/ZXSpectrum/Exporters/AssemblerExport.h
@@ -2,4 +2,6 @@
 
 struct FCodeAnalysisState;
 
-bool ExportAssembler(FCodeAnalysisState& state, const char* pTextFileName);
+// Export assembler to a file given an optional start address and end address.
+// If no start or end address are specified it will export from the the end of attrib memory to the top of RAM.
+bool ExportAssembler(FCodeAnalysisState& state, const char* pTextFileName, uint16_t startAddr = kScreenAttrMemEnd + 1, uint16_t endAddr=0xffff);
