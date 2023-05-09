fd43d470ad4f4bd7f747a697cb5d5cc8d801365b
TheGoodDoktor <mrmarkcraig@googlemail.com>
2023-05-09 21:24:28 +0100

    Fixes for test project

--- Source/ZXSpectrum/Tests/SpectrumTests.cpp
+++ Source/ZXSpectrum/Tests/SpectrumTests.cpp
@@ -2,6 +2,17 @@
 
 #include "CodeAnalyser/Tests/CodeAnalyserTests.h"
 
+#define SOKOL_IMPL
+#include <sokol_audio.h>
+
+void SetWindowTitle(const char* pTitle)
+{
+}
+
+void SetWindowIcon(const char* pIconFile)
+{
+}
+
 bool RunSpectrumTests()
 {
 	// TODO: Run tests on spectrum emu & code analyser
@@ -10,5 +21,11 @@ bool RunSpectrumTests()
 	bSuccess &= RunCodeAnalyserTests();
 
 	return bSuccess;
+}
+
+int main(int argc, char* argv[])
+{
+
+	return 0;
 }
 
