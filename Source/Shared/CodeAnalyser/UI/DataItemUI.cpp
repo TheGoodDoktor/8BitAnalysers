25669ebb4e6fbf36e4e97a55b7cc4a33df683950
Colourclash <sam@colourclash.co.uk>
2023-05-12 21:45:57 +0100

    Wrap text in details pane

--- Source/Shared/CodeAnalyser/UI/DataItemUI.cpp
+++ Source/Shared/CodeAnalyser/UI/DataItemUI.cpp
@@ -656,7 +656,7 @@ void DrawDataDetails(FCodeAnalysisState& state, FCodeAnalysisViewState& viewStat
 			else
 				textString += ch;
 		}
-		ImGui::Text("%s", textString.c_str());
+		ImGui::TextWrapped("%s", textString.c_str());
 		int length = pDataInfo->ByteSize;
 		if (ImGui::InputInt("Length", &length))
 		{
