e2af7280740cb31070fdbc4d7d58f082fbd11faa
TheGoodDoktor <TheGoodDoktor@users.noreply.github.com>
2024-01-07 13:15:22 +0000

    Updated base lua files

--- Data/SpectrumAnalyser/Lua/LuaBase.lua
+++ Data/SpectrumAnalyser/Lua/LuaBase.lua
@@ -1,4 +1,28 @@
 
+-- enumerate types
+
+EDataItemDisplayType = 
+{
+   Unknown = 0,
+   Pointer = 1,
+   JumpAddress = 2,
+   Decimal = 3,
+   Hex = 4,
+   Binary = 5,
+   Bitmap = 6,			-- 1 bit per pixel
+   ColMap2Bpp_CPC = 7,	-- Amstrad CPC mode 1 format
+   ColMap4Bpp_CPC = 8,	-- Amstrad CPC mode 0 format
+   ColMapMulticolour_C64 = 9,	-- C64 Multicolour format
+   UnsignedNumber = 10,
+   SignedNumber = 11
+}
+
+function SetDataItemTypeAndComment(address,displayType,comment)
+	SetDataItemComment(address,comment)		
+	SetDataItemDisplayType(address, displayType)
+end
+
+-- viewer related
 Viewers = {}
 
 function AddViewer(viewer)
@@ -27,134 +51,3 @@ function dumpFunc(o)
  function dump(o)
     print(dumpFunc(o))
  end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
