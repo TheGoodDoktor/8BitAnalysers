cf0c314bafdc2d2f76b8925f452feb68388ebf3c
Sam Hall <sam@colourclash.co.uk>
2024-02-02 18:40:54 +0000

    Change snapshot path in the package script

--- Scripts/MakeDistPackage.bat
+++ Scripts/MakeDistPackage.bat
@@ -3,16 +3,14 @@ echo off
 echo Deleting old package files...
 if exist ..\Package rmdir /s /q ..\Package
 
-mkdir ..\Package\Loose\Configs
-mkdir ..\Package\Loose\GameData
-mkdir ..\Package\Loose\Games
+mkdir ..\Package\Loose\SpectrumGames
 mkdir ..\Package\Loose\Pokes
 mkdir ..\Package\Loose\RZX
 mkdir ..\Package\Loose\Docs
 mkdir ..\Package\Loose\Fonts
 
 rem Add some files to describe what to put in the empty directories
-@echo>"..\Package\Loose\Games\48k and 128k snapshots go here - z80 or sna"
+@echo>"..\Package\Loose\SpectrumGames\48k and 128k snapshots go here - z80 or sna"
 @echo>"..\Package\Loose\Pokes\pok files go here - filename matching game name"
 @echo>"..\Package\Loose\RZX\rzx files go here"
 @echo>"..\Package\Loose\Fonts\fonts go here in ttf format"
