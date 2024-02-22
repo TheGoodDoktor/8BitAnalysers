7bd814068336ec33e437043b818ddca5ad5f9f63
Sam Hall <sam@colourclash.co.uk>
2024-02-22 10:01:01 +0000

    colour in lua cpc bitmap images

--- Source/CPC/CPCLuaAPI.cpp
+++ Source/CPC/CPCLuaAPI.cpp
@@ -47,16 +47,11 @@ static int DrawCPCImage(lua_State* pState, int mode)
 
 	const int xp = (int)luaL_optinteger(pState, 3, 0);
 	const int yp = (int)luaL_optinteger(pState, 4, 0);
-	const int widthPixels = (int)luaL_optinteger(pState, 5, 1);
-	const int heightPixels = (int)luaL_optinteger(pState, 6, 1);
-	const int paletteIndex = (int)luaL_optinteger(pState, 7, 1);
+	const int widthPixels = (int)luaL_optinteger(pState, 5, 8);
+	const int heightPixels = (int)luaL_optinteger(pState, 6, 8);
+	const int paletteIndex = (int)luaL_optinteger(pState, 7, 0);
 
-	if (mode == -1)
-	{
-		const uint32_t cols[] = { 0, 0xffffffff };
-		pGraphicsView->Draw1BppImageAt(pImageData, xp, yp, widthPixels, heightPixels, cols);	
-	}
-	else if (mode == 0)
+	if (mode == 0)
 		pGraphicsView->DrawMode0Image(pImageData, xp, yp, widthPixels, heightPixels, paletteIndex);
 	else
 		pGraphicsView->DrawMode1Image(pImageData, xp, yp, widthPixels, heightPixels, paletteIndex);
@@ -66,7 +61,43 @@ static int DrawCPCImage(lua_State* pState, int mode)
 
 static int DrawCPCBitImage(lua_State* pState)
 {
-	return DrawCPCImage(pState, -1);
+	FCPCGraphicsView* pGraphicsView = (FCPCGraphicsView*)lua_touserdata(pState, 1);
+	if (pGraphicsView == nullptr)
+		return 0;
+
+	const uint8_t* pImageData = (const uint8_t*)lua_touserdata(pState, 2);
+	if (pImageData == nullptr)
+		return 0;
+
+	const int xp = (int)luaL_optinteger(pState, 3, 0);
+	const int yp = (int)luaL_optinteger(pState, 4, 0);
+	const int widthPixels = (int)luaL_optinteger(pState, 5, 8);
+	const int heightPixels = (int)luaL_optinteger(pState, 6, 8);
+	const int paletteIndex = (int)luaL_optinteger(pState, 7, 0);
+	const int paperColIndex = (int)luaL_optinteger(pState, 8, 0);
+	const int inkColIndex = (int)luaL_optinteger(pState, 9, 1);
+
+	uint32_t cols[] = { 0, 0xffffffff };
+	
+	if (const FPaletteEntry* pPaletteEntry = GetPaletteEntry(paletteIndex))
+	{
+		const uint32_t* pPalette = GetPaletteFromPaletteNo(paletteIndex);
+		if (pPalette)
+		{
+			if (paperColIndex < pPaletteEntry->NoColours)
+			{
+				cols[0] = pPalette[paperColIndex];
+			}
+			if (inkColIndex < pPaletteEntry->NoColours)
+			{
+				cols[1] = pPalette[inkColIndex];
+			}
+		}
+	}
+
+	pGraphicsView->Draw1BppImageAt(pImageData, xp, yp, widthPixels, heightPixels, cols);
+
+	return 0;
 }
 
 static int DrawCPCMode0Image(lua_State *pState)
