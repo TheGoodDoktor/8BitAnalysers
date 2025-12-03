b2f0d8847d2d73e1128e9674f38c19472de87264
Sam Hall <sam@colourclash.co.uk>
2025-12-03 19:27:40 +0000

    add version of draw sprite that takes uint32_t*

--- Source/PCE/Viewers/PCEGraphicsView.cpp
+++ Source/PCE/Viewers/PCEGraphicsView.cpp
@@ -6,6 +6,63 @@
 // Monochrome sprites. Pass in as flag to Draw4bppSpriteImage()? Probably useless though.
 bool gPCEGraphicsViewMonoSprites = false;
 
+void FPCEGraphicsView::Draw4bppSpriteImage(const uint8_t* pSrc, int xp, int yp, int width, int height, const uint32_t* cols)
+{
+	constexpr int blockWidth = 16;
+	constexpr int blockHeight = 16;
+
+	int curXPos = xp;
+	int curYPos = yp;
+
+	const int viewWidth = GetWidth();
+	uint32_t* pPixBuf = GetPixelBuffer();
+
+	const uint16_t* pPlane0 = (uint16_t*)pSrc;
+
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			uint32_t* pCurPixBuf = pPixBuf + (curXPos + (curYPos * viewWidth));
+			const uint16_t* pPlane1 = pPlane0 + 16;
+			const uint16_t* pPlane2 = pPlane1 + 16;
+			const uint16_t* pPlane3 = pPlane2 + 16;
+
+			// Draw 16x16 pixel square
+			for (int y = 0; y < blockHeight; y++)
+			{
+				// Draw 16 pixel horiz line
+				for (int x = 0; x < blockWidth; x++)
+				{
+					const int bit = (blockWidth - 1) - x;
+					// Get the 4 bit pixel colour index (0-15)
+					const int colour = ((*pPlane3 >> bit) & 1) << 3 | ((*pPlane2 >> bit) & 1) << 2 | ((*pPlane1 >> bit) & 1) << 1 | ((*pPlane0 >> bit) & 1) & 0xf;
+
+					if (colour != 0) // 0 is transparent
+					{
+						*pCurPixBuf = cols[colour];
+					}
+					else
+						*pCurPixBuf = 0xff000000;
+
+					pCurPixBuf++;
+				}
+
+				pPlane0++;
+				pPlane1++;
+				pPlane2++;
+				pPlane3++;
+
+				pCurPixBuf += viewWidth - blockWidth;
+			}
+			curXPos += blockWidth;
+			pPlane0 = pPlane3;
+		}
+		curYPos += blockHeight;
+		curXPos = xp;
+	}
+}
+
 // height and width are in 16x16 sprite character blocks.
 // paletteIndex is sprite palette index 0-15
 void FPCEGraphicsView::Draw4bppSpriteImage(const uint8_t* pSrc, int xp, int yp, int width, int height, int paletteIndex)
@@ -15,7 +72,7 @@ void FPCEGraphicsView::Draw4bppSpriteImage(const uint8_t* pSrc, int xp, int yp, 
 	HuC6260* huc6260 = pCore->GetHuC6260();
 	u16* colorTable = huc6260->GetColorTable();
 	constexpr int paletteBaseIndex = 0x100;
-	const u16* pPalette = &colorTable[paletteBaseIndex + (paletteIndex * 16)];
+	const u16* pPalette = &colorTable[paletteBaseIndex + ((paletteIndex & 0xf) * 16)];
 
 	constexpr int blockWidth = 16;
 	constexpr int blockHeight = 16;
@@ -45,7 +102,7 @@ void FPCEGraphicsView::Draw4bppSpriteImage(const uint8_t* pSrc, int xp, int yp, 
 				{
 					const int bit = (blockWidth - 1) - x;  
 					// Get the 4 bit pixel colour index (0-15)
-					const int colour = ((*pPlane3 >> bit) & 1) << 3 | ((*pPlane2 >> bit) & 1) << 2 | ((*pPlane1 >> bit) & 1) << 1 | ((*pPlane0 >> bit) & 1) & 0x1ff;
+					const int colour = ((*pPlane3 >> bit) & 1) << 3 | ((*pPlane2 >> bit) & 1) << 2 | ((*pPlane1 >> bit) & 1) << 1 | ((*pPlane0 >> bit) & 1) & 0xf;
 
 					if (colour != 0) // 0 is transparent
 					{
