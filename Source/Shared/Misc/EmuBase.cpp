0c2f2a6fd177bf6ab52dd29aa014760fd914eb2d
Sam Hall <sam@colourclash.co.uk>
2026-05-21 23:01:35 +0100

    make debugger keys work when any window is active

--- Source/Shared/Misc/EmuBase.cpp
+++ Source/Shared/Misc/EmuBase.cpp
@@ -231,6 +231,7 @@ void FEmuBase::DrawUI()
 
 	// Code analysis views
 	CodeAnalysis.UpdateFocussedViewState();
+	ProcessDebuggerKeyCommands(CodeAnalysis);
 	for (int codeAnalysisNo = 0; codeAnalysisNo < FCodeAnalysisState::kNoViewStates; codeAnalysisNo++)
 	{
 		char name[32];
