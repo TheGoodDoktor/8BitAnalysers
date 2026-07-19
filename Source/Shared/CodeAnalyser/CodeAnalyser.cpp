a87b82afad668b9f8fa5c8a07ababafc7da8a32b
Sam Hall <sam@colourclash.co.uk>
2026-07-19 17:50:59 +0100

    make zp_XX names use a byte instead of word

--- Source/Shared/CodeAnalyser/CodeAnalyser.cpp
+++ Source/Shared/CodeAnalyser/CodeAnalyser.cpp
@@ -845,10 +845,15 @@ FLabelInfo* GenerateLabelForAddress(FCodeAnalysisState &state, FAddressRef addre
 				snprintf(label, kLabelSize, "data_%s_%04X", pBank ? pBank->Name.c_str() : "unknown", address.GetAddress());
 
 			// zero page labels for 6502
-			if ((state.CPUInterface->CPUType == ECPUType::M6502 && address.GetAddress() < 256) ||
-			   (state.CPUInterface->CPUType == ECPUType::HuC6280 && address.GetAddress() >= 0x2000 && address.GetAddress() < 0x2100))
+			if (state.CPUInterface->CPUType == ECPUType::M6502)
 			{
-				snprintf(label, kLabelSize, "zp_%02X", address.GetAddress());
+				if (address.GetAddress() < 256)
+					snprintf(label, kLabelSize, "zp_%02X", address.GetAddress());
+			}
+			else if (state.CPUInterface->CPUType == ECPUType::HuC6280)
+			{
+				if (address.GetAddress() >= 0x2000 && address.GetAddress() < 0x2100)
+					snprintf(label, kLabelSize, "zp_%02X", address.GetAddress() - 0x2000);
 			}
 			if (bLabelOnOperand == false)
 				pLabel->Global = true;	// operand labels should be local
